a4acb83a70cd5416e4de3a0702bd5dcd891c4442
George <cortes.george93@icloud.com>
2024-02-08 23:50:33 -0800

    Constructor added for Todo Class

---
+++ todo.js
@@ -0,0 +1,12 @@
+export default class Todo{
+    // Create a todo list article that has a title description, duedate and priority.
+    constructor(title,description,dueDate,priority){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.check = false; // assume the todo has not been completed
+    }
+    
+    
+}
